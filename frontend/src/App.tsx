6f8b5fe663aef4489d40d754e42c609f4061853a
yeonsu <yeonsu4610@gmail.com>
2026-05-26 23:35:50 +0900

    feat : Dockerfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage';
 import CoursesPage from './CoursesPage/CoursesPage';
